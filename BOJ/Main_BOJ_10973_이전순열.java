9cdc34932cd3224a526003556a2ffb7d50724d36
mmmmjjj <myungju25@naver.com>
2021-09-11 21:19:01 +0900

    2021.09.11

---
+++ BOJ/Main_BOJ_10973_이전순열.java
@@ -0,0 +1,70 @@
+package sw_기초.브루트포스;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_BOJ_10973_이전순열 {
+
+	static int N;
+	static int[] perm;
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		N = Integer.parseInt(br.readLine());
+
+		perm = new int[N];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			perm[i] = Integer.parseInt(st.nextToken());
+		}
+
+		if (np(perm)) {
+
+			for (int i = 0; i < perm.length; i++) {
+				bw.write(Integer.toString(perm[i]));
+				bw.write(' ');
+			}
+		}
+		
+		else {
+			bw.write("-1");
+		}
+		
+		bw.flush();
+		bw.close();
+
+	}
+
+	private static boolean np(int[] perm) {
+
+		int n = perm.length;
+
+		int i = n - 1;
+		while (i > 0 && perm[i - 1] <= perm[i]) --i;
+		
+		if (i == 0) return false;
+
+		int j = n - 1;
+
+		while (perm[i - 1] <= perm[j]) --j;
+
+		swap(perm, i - 1, j);
+
+		int k = n - 1;
+		while (i < k) swap(perm, i++, k--);
+
+		return true;
+	}
+
+	private static void swap(int[] perm, int i, int j) {
+		int temp = perm[i];
+		perm[i] = perm[j];
+		perm[j] = temp;
+
+	}
+}
